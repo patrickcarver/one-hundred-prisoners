4fa6a9590b6c6dc9521b551e9b8d51f7cde1859e
Patrick Carver <patrickcarver@gmail.com>
2019-08-03 12:06:25 -0500

    it works

--- lib/one_hundred_prisoners.ex
+++ lib/one_hundred_prisoners.ex
@@ -14,10 +14,41 @@ defmodule OneHundredPrisoners do
   # turn the light off the first time they find it on;
   # otherwise leave it in the state it is found
 
- # def run do
- #   turn(light_bulb: :off, prisoners: create_prisoners())
- # end
+  def run do
+    turn(light_bulb: :off, prisoners: create_prisoners())
+  end
 
+  def turn(light_bulb: state, prisoners: prisoners) do
+    {index, prisoner} = Enum.random(prisoners)
+
+    {new_state, new_prisoner} = process(state, prisoner)
+
+    case new_prisoner do
+      %{type: :counter, times_on_to_off: 100} ->
+        IO.puts "Finished"
+        prisoners
+      _ ->
+        new_prisoners = Map.put(prisoners, index, new_prisoner)
+        turn(light_bulb: new_state, prisoners: new_prisoners)
+    end
+  end
+
+  def process(:off, %{type: :counter, times_on_to_off: count}) do
+    new_prisoner = %{type: :counter, times_on_to_off: count + 1}
+    {:on, new_prisoner}
+  end
+
+  def process(:on, %{type: :counter} = prisoner) do
+    {:on, prisoner}
+  end
+
+  def process(:on, %{type: :regular, have_found_light_on_once: false}) do
+    {:off, %{type: :regular, have_found_light_on_once: true}}
+  end
+
+  def process(state, prisoner) do
+    {state, prisoner}
+  end
 
   def create_prisoners() do
     1..99
