9a5fb465f13b925b7b94ad27ef7f6a6b2e34eb6a
Patrick Carver <patrickcarver@gmail.com>
2019-08-04 23:36:08 -0500

    change visit function to use pipelines

--- lib/one_hundred_prisoners.ex
+++ lib/one_hundred_prisoners.ex
@@ -19,35 +19,46 @@ defmodule OneHundredPrisoners do
   end
 
   defp visit(state) do
-    {index, prisoner} = Enum.random(state.prisoners)
-
-    {new_light_bulb, new_prisoner} = Prisoner.visit_room(state.light_bulb, prisoner)
-
-    case new_prisoner do
-      %Counter{times_on_to_off: 100} ->
-        verdict(state)
-      _ ->
-        new_prisoners = Map.put(state.prisoners, index, new_prisoner)
-        new_state = update(state, new_light_bulb, new_prisoners, index)
-        visit(new_state)
-    end
+    state
+    |> random_prisoner_with_id()
+    |> visit_room()
+    |> verdict_or_visit()
   end
 
+  defp random_prisoner_with_id(state) do
+    {id, prisoner} = Enum.random(state.prisoners)
+    {state, id, prisoner}
+  end
 
-  defp update(state, light_blub, prisoners, index) do
+  defp visit_room({state, id, prisoner}) do
+    {new_light_bulb, new_prisoner} = Prisoner.visit_room(state.light_bulb, prisoner)
+    {state, new_prisoner, new_light_bulb, id}
+  end
+
+  defp verdict_or_visit({state, %Counter{times_on_to_off: 100}, _light_bulb, _id}) do
+    num_visited = Enum.count(state.ids_of_visited)
+    verdict(num_visited)
+  end
+
+  defp verdict_or_visit({state, prisoner, light_bulb, id}) do
+    new_prisoners = Map.put(state.prisoners, id, prisoner)
+    new_state = update(state, new_prisoners, light_bulb, id)
+    visit(new_state)
+  end
+
+  defp update(state, prisoners, light_bulb, id) do
     state
-    |> Map.put(:light_bulb, light_blub)
+    |> Map.put(:light_bulb, light_bulb)
     |> Map.put(:prisoners, prisoners)
     |> Map.update!(:current_visit, & &1 + 1)
-    |> Map.update!(:ids_of_visited, & MapSet.put(&1, index))
+    |> Map.update!(:ids_of_visited, & MapSet.put(&1, id))
   end
 
-  defp verdict(state) do
-    num_visited = Enum.count(state.ids_of_visited)
-    if num_visited == 100 do
-      :set_free
-    else
-      :shoot_them
-    end
+  defp verdict(num_visited) when num_visited == 100 do
+    :set_free
+  end
+
+  defp verdict(_num_visited) do
+    :shoot_them
   end
 end
